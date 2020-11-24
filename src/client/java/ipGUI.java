8cbacdcaa68aa68018787cb40aedd1d0e43e37b2
SomethinSomethin <56287943+SomethinSomethin@users.noreply.github.com>
2020-11-24 17:15:20 +0700

    Create ipGUI.java

---
+++ src/client/java/ipGUI.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class ipGUI extends JFrame implements ActionListener {
+    private JTextField tf;
+    private JButton b;
+    private Label l;
+
+    public ipGUI(){
+        tf = new JTextField();
+        b = new JButton("Submit");
+        b.addActionListener(this);
+        l = new Label("Enter ip");
+        this.setLayout(new BorderLayout());
+
+        this.add(l, BorderLayout.NORTH);
+        this.add(tf, BorderLayout.CENTER);
+        this.add(b, BorderLayout.SOUTH);
+        
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(b)){
+            SettingClient.setSERVERIP(tf.getText());
+        }
+    }
+}
