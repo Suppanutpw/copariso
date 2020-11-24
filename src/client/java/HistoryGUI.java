da758fe3dd7162c0957d7e48a8f72ec8644559f6
SomethinSomethin <56287943+SomethinSomethin@users.noreply.github.com>
2020-11-24 09:38:42 +0700

    Create HistoryGUI.java

---
+++ src/client/java/HistoryGUI.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class HistoryGUI extends JFrame implements WindowListener {
+    private JPanel bg;
+    private JScrollPane sp;
+    private JTable t;
+    private  final String head[] = {"Date", "Old Path", "New Path", "Directory"};
+
+    public HistoryGUI(){
+        bg = new JPanel(new FlowLayout());
+        this.add(bg);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.addWindowListener(this);
+    }
+
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        t = new JTable();
+        sp = new JScrollPane();
+        sp.add(t);
+        bg.add(sp);
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {}
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
