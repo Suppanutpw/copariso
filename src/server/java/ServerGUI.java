3e5345bd27fdb5877f7d83ba12964fe338b699f2
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2020-11-24 14:30:17 +0700

    update gui

--- src/server/java/ServerGUI.java
+++ src/server/java/ServerGUI.java
@@ -2,28 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ServerGUI extends JFrame{
     private JPanel panel1;
     private JButton STARTButton;
     private JButton STOPButton;
-    private JTextArea line;
     private JLabel server;
     private JLabel status;
     private JPanel panel2;
     private JPanel panel3;
     private JPanel panel4;
+    private JTextArea textArea1;
 
     public ServerGUI(){
-        JTextArea line = new JTextArea();
         setTitle("Compariso");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(450, 300);
+        setSize(850, 300);
+        setLocationRelativeTo(null);
+
+        textArea1.setEditable(false);
 
         STARTButton.addActionListener(new ServerGUI.Start());
         STOPButton.addActionListener(new ServerGUI.Stop());
 
         status.setForeground(Color.RED);
+
+
 
         panel2.setBackground(null);
         panel3.setBackground(null);
@@ -32,15 +38,30 @@ public class ServerGUI extends JFrame{
         panel1.setBackground(Color.getHSBColor(26,70,91));
         add(panel1);
 
+        //show log
+        FileReader reader = null;
+        try {
+            reader = new FileReader("serverLog.log");
+            textArea1.read(reader, "serverLog.log");
+        } catch (IOException exception) {
+            System.err.println("Load oops");
+            exception.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException exception) {
+                    System.err.println("Error closing reader");
+                    exception.printStackTrace();
+                }
+            }
+        }
+        pack();
         setVisible(true);
     }
 
     public static void main(String[] args){
         new ServerGUI();
-    }
-
-    public void setLine(String newText) {
-        line.append(newText);
     }
 
 
